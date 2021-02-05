168b1b9e19659e7e3155b2f3f01120172a539417
Eric <enriquemwenda5@gmail.com>
2021-02-05 17:29:01 +0300

    Description Layout Added

--- app/src/main/java/com/kimadrian/kazilink/SignUp.java
+++ app/src/main/java/com/kimadrian/kazilink/SignUp.java
@@ -1,9 +1,12 @@
 package com.kimadrian.kazilink;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -12,10 +15,19 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class SignUp extends AppCompatActivity {
 
@@ -23,6 +35,10 @@ public class SignUp extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText userNameEditText, emailEditText, phoneNumberEditText, professionEditText,
             passwordEditText, confirmPasswordEditText;
+    private Uri imageUri;
+    public FirebaseStorage storage;
+    public StorageReference storageReference;
+    public boolean hasUserUploadedPicture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +53,8 @@ public class SignUp extends AppCompatActivity {
         confirmPasswordEditText = findViewById(R.id.editTextTextPassword3);
 
         mAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
     }
 
     public void submitButtonClicked(View view){
@@ -95,6 +113,10 @@ public class SignUp extends AppCompatActivity {
             Toast.makeText(this, "Passwords do not Match!", Toast.LENGTH_SHORT).show();
             return;
         }
+        /*if(!hasUserUploadedPicture){
+            Toast.makeText(this, "Please Upload a Profile Picture.", Toast.LENGTH_SHORT).show();
+            return;
+        }*/
 
         myProgressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -117,16 +139,69 @@ public class SignUp extends AppCompatActivity {
                                         startActivity(new Intent(SignUp.this, Login.class));
                                     }
                                     else{
-                                        Toast.makeText(SignUp.this, "Registration Failure", Toast.LENGTH_LONG).show();
+                                        Toast.makeText(SignUp.this, "Task 2 Not Successful Registration Failure", Toast.LENGTH_LONG).show();
                                         myProgressBar.setVisibility(View.GONE);
                                     }
                                 }
                             });
                         }
                         else{
-                            Toast.makeText(SignUp.this, "Registration Failure", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignUp.this, "Task 1 not successful Registration Failure", Toast.LENGTH_LONG).show();
                             myProgressBar.setVisibility(View.GONE);
                         }
+                    }
+                });
+    }
+
+    public void uploadProfilePicture(View view){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+            uploadPictureToFirebase();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Select an Image.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void uploadPictureToFirebase(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading Image...");
+        progressDialog.show();
+        final String randomKey = UUID.randomUUID().toString();
+
+        StorageReference myStorageReference  = storageReference.child("images" + randomKey);
+        myStorageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressDialog.dismiss();
+                        Snackbar.make(findViewById(android.R.id.content), "Profile Picture Uploaded" , Snackbar.LENGTH_LONG).show();
+                        //hasUserUploadedPicture = true;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Unable to Upload Picture", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progressPercentage = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                        progressDialog.setMessage("Progress : " + (int)progressPercentage + "%");
                     }
                 });
     }
